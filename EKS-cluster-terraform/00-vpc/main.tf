1bd75976567ea8c62b7d004c4547100838769b75
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-01-23 12:11:14 +0530

    kubernetes

--- EKS-cluster-terraform/00-vpc/main.tf
+++ EKS-cluster-terraform/00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "main" {
-  source = "git::git@github.com:Royal-Reddy-Yaparla/practice.git?ref=main"
+  source = "git::https://github.com/Royal-Reddy-Yaparla/terraform-vpc-aws-module?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
@@ -9,7 +9,7 @@ module "main" {
   cidr_public = var.cidr_public
   cidr_private = var.cidr_private
   cidr_database = var.cidr_database
-  
+
   is_peering_required = var.is_peering_required
   acceptor_vpc_id = var.accepters_vpc_id
 }
