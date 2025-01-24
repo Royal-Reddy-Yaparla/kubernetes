4e18da126c9b5b7705a51210804239c0bdd49098
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-01-24 10:35:50 +0530

    k8

--- EKS-cluster-terraform/00-vpc/parameters.tf
+++ EKS-cluster-terraform/00-vpc/parameters.tf
@@ -1,25 +1,25 @@
-resource "aws_ssm_parameter" "vpc_id" {
-    name = "/${var.project_name}/${var.environment}/vpc_id"
-    type = "String"
-    value = module.robokart.vpc_id
-}
+# resource "aws_ssm_parameter" "vpc_id" {
+#     name = "/${var.project_name}/${var.environment}/vpc_id"
+#     type = "String"
+#     value = module.robokart.vpc_id
+# }
 
-resource "aws_ssm_parameter" "public_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}/public_subnet_ids"
-    type = "StringList"
-    value = join(",", module.robokart.public_subnet_ids)
-}
+# resource "aws_ssm_parameter" "public_subnet_ids" {
+#     name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.robokart.public_subnet_ids)
+# }
 
-resource "aws_ssm_parameter" "private_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}/private_subnet_ids"
-    type = "StringList"
-    value = join(",", module.robokart.private_subnet_ids)
-}
+# resource "aws_ssm_parameter" "private_subnet_ids" {
+#     name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.robokart.private_subnet_ids)
+# }
 
-resource "aws_ssm_parameter" "database_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}/database_subnet_ids"
-    type = "StringList"
-    value = join(",", module.robokart.database_subnet_ids)
-}
+# resource "aws_ssm_parameter" "database_subnet_ids" {
+#     name = "/${var.project_name}/${var.environment}/database_subnet_ids"
+#     type = "StringList"
+#     value = join(",", module.robokart.database_subnet_ids)
+# }
 
 
